ec58086e28d4e2c2e1d97f5ceb1333e91f37e46e
JacobWarthen <jcwarthen@dons.usfca.edu>
2023-01-27 11:05:12 -0800

    Update HelloWorld.java
    Added helpful comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 //Simple Hello World program in Java
 public class HelloWorld {
     public static void main(String[] args) {
+        //Print Hello World!
         System.out.println("Hello World!");
     }
 }
